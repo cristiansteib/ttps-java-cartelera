f2c1563f6523d423f9bc5450f57dacdf7b96ed15
Sebastian Muiña <sebastiangabrielm@gmail.com>
2018-10-19 17:21:14 -0300

    notification and user

--- src/classDAO/User.java
+++ src/classDAO/User.java
@@ -3,6 +3,53 @@ package classDAO;
 import java.io.Serializable;
 
 public class User implements Serializable {
+
     private String name;
     private String lastName;
+    private String password;
+    private String DNI;
+    private Notification notification;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public Notification getNotification() {
+        return notification;
+    }
+
+    public void setNotification(Notification notification) {
+        this.notification = notification;
+    }
+
+
+
 }
