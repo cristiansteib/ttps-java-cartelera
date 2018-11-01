6865c8e2ad38874924460138f1ddf5f16e60e28e
Cristian Steib <cristiansteib@gmail.com>
2018-11-01 20:55:04 -0300

    dbconnector

--- src/dao/impl/hibernate/DAOHibernateImplementation.java
+++ src/dao/impl/hibernate/DAOHibernateImplementation.java
@@ -9,45 +9,38 @@ import java.util.Collection;
 import java.util.List;
 
 abstract public class DAOHibernateImplementation<T, ID> implements DAO<T, ID> {
-    DBconnection db;
-    EntityManagerFactory emf;
-    EntityManager em;
-    EntityTransaction etx;
+    private DBconnection db;
 
     abstract String getModelName();
 
     DAOHibernateImplementation() {
-        DBconnection db = DBconnection.getConnection();
-        emf = db.emf;
-        em = db.em;
-        etx = db.etx;
+        this.db = DBconnection.getConnection();
     }
 
-    public T getById(Class <T> typo,ID id) {
-        return this.em.find(typo, id);
+    public T getById(Class<T> typo, ID id) {
+        return this.db.em.find(typo, id);
     }
 
-    public List<T> findAll () {
-        return em.createQuery("SELECT e FROM "+ this.getModelName() + " e").getResultList();
+    public List<T> findAll() {
+        return this.db.em.createQuery("SELECT e FROM " + this.getModelName() + " e").getResultList();
     }
 
     public void create(T entity) {
-        etx.begin();
-        this.em.persist(entity);
-        etx.commit();
+        this.db.etx.begin();
+        this.db.em.persist(entity);
+        this.db.etx.commit();
     }
 
     public T update(T entity) {
-        etx.begin();
-        this.em.merge(entity);
-        etx.commit();
+        this.db.etx.begin();
+        this.db.em.merge(entity);
+        this.db.etx.commit();
         return entity;
     }
 
     public void remove(T entity) {
-        etx.begin();
-        this.em.remove(entity);
-        etx.commit();
+        this.db.etx.begin();
+        this.db.em.remove(entity);
+        this.db.etx.commit();
     }
-
 }
