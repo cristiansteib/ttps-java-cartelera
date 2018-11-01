e183191f1635acd9698a2c35e858b4f099079165
Cristian Steib <cristiansteib@gmail.com>
2018-11-01 19:23:28 -0300

    Added test folder

--- src/test/java/dao/impl/hibernate/UserDAOTest.java
+++ src/test/java/dao/impl/hibernate/UserDAOTest.java
@@ -1,5 +1,27 @@
+package dao.impl.hibernate;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UserDAOTest {
 
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("ksdfjsklfj");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getModelClass() {
+    }
+
+    @Test
+    public void findAll() {
+    }
 }
