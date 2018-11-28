a28a832cbd0d4a2bc9478847baaf82ec67005b9d
Cristian Steib <cristiansteib@gmail.com>
2018-11-27 23:27:16 -0300

    new controller

---
+++ src/ttps/spring/controllers/UserController.java
@@ -0,0 +1,22 @@
+package ttps.spring.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ttps.spring.model.User;
+
+
+@RestController
+public class UserController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/usuario")
+    public User greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        User user = new User();
+
+        return user;
+    }
+}
