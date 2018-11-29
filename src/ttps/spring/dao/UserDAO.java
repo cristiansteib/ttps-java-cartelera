a563f3aa81224cf1667e8ab717d959240dace623
Sebastian Muiña <sebastiangabrielm@gmail.com>
2018-11-28 21:36:43 -0300

    userdao

--- src/ttps/spring/dao/UserDAO.java
+++ src/ttps/spring/dao/UserDAO.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Repository;
 import sun.swing.StringUIClientPropertyKey;
 import ttps.spring.model.User;
 
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 @Repository
 public class UserDAO extends DaoImplementation<User, Integer> {
 
@@ -13,8 +17,17 @@ public class UserDAO extends DaoImplementation<User, Integer> {
 
     public User login(String username, String password) {
         /* Return a user instance with the given username and password, if not found return null*/
-        User user = new User();
-        return user;
+        try {
+            String queryString = "SELECT u FROM User u where userName = :username AND password = :password";
+            TypedQuery<User> query = getEntityManager().createQuery(queryString, User.class);
+            query.setParameter("username", username);
+            query.setParameter("password", password);
+            User result = query.getSingleResult();
+            return (result);
+        } catch (NoResultException e) {
+            return null;
+        }
+
     }
 }
 
