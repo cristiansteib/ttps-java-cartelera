5257e7ad2891fadaec9d59b85887543e38fbbb8d
Cristian Steib <cristian.steib@avature.net>
2018-11-29 20:42:45 -0300

    Don't send the password in the user json

--- src/ttps/spring/model/User.java
+++ src/ttps/spring/model/User.java
@@ -1,9 +1,11 @@
 package ttps.spring.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.io.Serializable;
-
+@JsonIgnoreProperties(value= {"password"})
 @Entity
 public class User implements Serializable {
 
